0ca26c2c1dcdf5a67c1b7f527cfc64cab02b43f4
SYDNEY GRAHAM <SLG22F@FSU.EDU>
2023-09-19 22:35:30 -0400

    Stopped until Mouse press

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
+let x = 200;
+let speed = 50;
+let diameter = 40;
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1250, 600);
 }
 
 function draw() {
-  background(125);
+  background("lavender");
+  ellipse( x, height/2, diameter);
+  if (x >= width || x <= 0) {
+    speed= speed * -1;
+    
+  }
+  x+= speed;
+}
+function mousePressed() {
+  noLoop();
+}
+
+function mouseReleased() {
+  loop();
 }
